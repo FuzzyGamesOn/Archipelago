ec9728beaf404aa12ac300068a6b7953565f411e
Sam Zimmerman <samzimmerman@me.com>
2023-12-05 19:46:39 -0800

    Fix broken helper Add "Avengers" dev category

--- worlds/_manual/Helpers.py
+++ worlds/_manual/Helpers.py
@@ -16,3 +16,4 @@ def category_join(table: List[Dict[str, any]]):
     for item in table:
         if len({'category', 'dev_category'}.intersection(item.keys())):
             item['all_category'] = item.get('category', []) + item.get('dev_category', [])
+    return table
